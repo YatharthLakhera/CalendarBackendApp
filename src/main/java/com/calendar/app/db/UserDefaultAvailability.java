ebf47e84f7fba927b9c52137c749f48d14b0b441
YatharthLakhera <17159994+YatharthLakhera@users.noreply.github.com>
2024-11-09 11:36:18 +0530

    Handling null values

--- src/main/java/com/calendar/app/db/UserDefaultAvailability.java
+++ src/main/java/com/calendar/app/db/UserDefaultAvailability.java
@@ -5,6 +5,7 @@ import com.calendar.app.enums.TimeZone;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,13 +32,13 @@ public class UserDefaultAvailability {
     @JsonIgnore
     public List<String> getWeeksAvailability() {
         List<String> weeksAvailability = new ArrayList<>();
-        weeksAvailability.add(this.monday);
-        weeksAvailability.add(this.tuesday);
-        weeksAvailability.add(this.wednesday);
-        weeksAvailability.add(this.thursday);
-        weeksAvailability.add(this.friday);
-        weeksAvailability.add(this.saturday);
-        weeksAvailability.add(this.sunday);
+        weeksAvailability.add(StringUtils.isBlank(this.monday) ? "" : this.monday);
+        weeksAvailability.add(StringUtils.isBlank(this.tuesday) ? "" : this.tuesday);
+        weeksAvailability.add(StringUtils.isBlank(this.wednesday) ? "" : this.wednesday);
+        weeksAvailability.add(StringUtils.isBlank(this.thursday) ? "" : this.thursday);
+        weeksAvailability.add(StringUtils.isBlank(this.friday) ? "" : this.friday);
+        weeksAvailability.add(StringUtils.isBlank(this.saturday) ? "" : this.saturday);
+        weeksAvailability.add(StringUtils.isBlank(this.sunday) ? "" : this.sunday);
         return weeksAvailability;
     }
 }
